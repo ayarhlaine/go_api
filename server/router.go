17644edbe33abb25eef664e5078dd9c3fca97fe7
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2022-09-12 12:19:52 +0630

    feat: rendered static html files

--- server/router.go
+++ server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/ayarhlaine/go_api/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -8,6 +10,16 @@ import (
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 
+	// Render static files
+	router.Static("/assets", "./static/assets")
+	router.LoadHTMLGlob("static/*.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"content": "This is an index page...",
+		})
+	})
+
+	// API Routes
 	router.GET("/ping", controllers.Pong)
 
 	return router
