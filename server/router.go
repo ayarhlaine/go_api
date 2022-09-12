f3fe0c4135abc4b727b6ee933c8726d047c279f6
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2022-09-12 11:41:08 +0630

    fix: fixed wrong imports

--- server/router.go
+++ server/router.go
@@ -1,14 +1,14 @@
 package server
 
 import (
-	"github.com/ayarhlaine/go_api/controllers/ping"
+	"github.com/ayarhlaine/go_api/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/ping", ping.Pong)
+	router.GET("/ping", controllers.Pong)
 
 	return router
 }
